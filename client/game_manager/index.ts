e7dcf21494f712f2ffecce991c254f274d38222d
Jim Hessin <jhessin@gmail.com>
2021-01-31 21:16:03 -0700

    Removed old client code.

--- client/game_manager/index.ts
+++ client/game_manager/index.ts
@@ -1,14 +1,12 @@
-import GameManager from './GameManager';
-import ChestModel from './ChestModel';
-import MonsterModel from './MonsterModel';
-import PlayerModel from './PlayerModel';
-import Spawner from './Spawner';
+import ChestModel from '../../server/game_manager/ChestModel';
+import MonsterModel from '../../server/game_manager/MonsterModel';
+import PlayerModel from '../../server/game_manager/PlayerModel';
+import Spawner from '../../server/game_manager/Spawner';
 
-export * from './Spawner';
+export * from '../../server/game_manager/Spawner';
 export * from './utils';
 
 export {
-  GameManager as default,
   ChestModel,
   MonsterModel,
   PlayerModel,
